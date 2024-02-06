5d231b2afedb1cd23a9920ecc3b2ff37e517b12c
HodayaGitHub <hodaya1812@gmail.com>
2024-02-06 11:23:22 +0200

    Refactor - split query function into query and buildCriteria functions

--- api/stay/stay.service.js
+++ api/stay/stay.service.js
@@ -16,133 +16,69 @@ export const stayService = {
 
 // const ITEMS_PER_PAGE = 48
 
-// async function query(filterBy, page = 1, itemsPerPage) {
-//     try {
-//         const criteria = {}
-//         const collection = await dbService.getCollection('stay')
-
-
-//         if (filterBy?.label) {
-//             criteria.$or = [
-//                 { "type": filterBy.label },
-//                 { "amenities": { $elemMatch: { $eq: filterBy.label } } }
-//             ]
-//         }
-
-//         if (filterBy?.roomType && filterBy.roomType !== "Any type") {
-//             criteria.roomType = filterBy.roomType
-//         }
-
-//         if (filterBy?.price) {
-//             criteria.price = {
-//                 $gte: +filterBy.price.minPrice,
-//                 $lte: +filterBy.price.maxPrice
-//             }
-//         }
-
-//         if (filterBy?.beds) {
-//             criteria.beds = { $gte: +filterBy.beds }
-//         }
-
-//         if (filterBy?.bathrooms) {
-//             criteria.bathrooms = { $gte: +filterBy.bathrooms }
-//         }
-
-//         if (filterBy?.bedrooms) {
-//             criteria.bedrooms = { $gte: +filterBy.bedrooms }
-//         }
-
-//         if (filterBy?.region) {
-//             console.log(filterBy.region)
-//             criteria['loc.country'] = filterBy.region
-//         }
-
-//         if (filterBy?.guestFavorite !== "false") {
-//             console.log(filterBy.guestFavorite)
-//             criteria.guestFavorite = { $ne: false }
-//         }
-
-
-
-//         let skips = +itemsPerPage * (page - 1)
-//         let totalDocumentsCount = await collection.find(criteria).toArray()
-//         let stays = await collection.find(criteria).skip(skips).limit(+itemsPerPage).toArray();
-
-//         console.log('totalDocumentsCount ', totalDocumentsCount.length);
-//         return { totalDocumentsCount, stays }
-
-//         // return stays
-//     } catch (err) {
-//         logger.error('Cannot find stays', err)
-//         throw err
-//     }
-// }
-
-
 async function query(filterBy, page = 1, itemsPerPage) {
     try {
-        const collection = await dbService.getCollection('stay')
-        const criteria = buildCriteria(filterBy)
-        let stays
-        let skips = +itemsPerPage * (page - 1)
-        const totalStayCount = await collection.countDocuments(criteria);
-        stays = await collection.find(criteria).sort({ _id: -1 }).skip(skips).limit(+itemsPerPage).toArray();
-        return { totalStayCount, stays }
+        const criteria = buildCriteria(filterBy);
+        const collection = await dbService.getCollection('stay');
+
+        let skips = +itemsPerPage * (page - 1);
+        let totalDocumentsCount = await collection.find(criteria).toArray();
+        let stays = await collection.find(criteria).skip(skips).limit(+itemsPerPage).toArray();
+
+        console.log('totalDocumentsCount ', totalDocumentsCount.length);
+        return { totalDocumentsCount, stays };
     } catch (err) {
-        logger.error('Cannot find stays', err)
-        throw err
+        logger.error('Cannot find stays', err);
+        throw err;
     }
 }
 
+function buildCriteria(filterBy) {
+    const criteria = {};
 
-function buildCriteria() {
-    const criteria = {}
     if (filterBy?.label) {
         criteria.$or = [
             { "type": filterBy.label },
             { "amenities": { $elemMatch: { $eq: filterBy.label } } }
-        ]
+        ];
     }
+
     if (filterBy?.roomType && filterBy.roomType !== "Any type") {
-        criteria.roomType = filterBy.roomType
+        criteria.roomType = filterBy.roomType;
     }
+
     if (filterBy?.price) {
-        if (+filterBy.price.minPrice >= 0) {
-            criteria.price = {
-                $gte: +filterBy.price.minPrice
-            }
-        }
-        if (+filterBy.price.maxPrice !== Infinity) {
-            criteria.price = criteria.price ?? {}
-            criteria.price.$lte = +filterBy.price.maxPrice
-        }
+        criteria.price = {
+            $gte: +filterBy.price.minPrice,
+            $lte: +filterBy.price.maxPrice
+        };
     }
+
     if (filterBy?.beds) {
-        criteria.beds = { $gte: +filterBy.beds }
+        criteria.beds = { $gte: +filterBy.beds };
     }
+
     if (filterBy?.bathrooms) {
-        criteria.bathrooms = { $gte: +filterBy.bathrooms }
+        criteria.bathrooms = { $gte: +filterBy.bathrooms };
     }
+
     if (filterBy?.bedrooms) {
-        criteria.bedrooms = { $gte: +filterBy.bedrooms }
+        criteria.bedrooms = { $gte: +filterBy.bedrooms };
     }
+
     if (filterBy?.region) {
-        console.log(filterBy.region)
-        criteria['loc.country'] = filterBy.region
+        console.log(filterBy.region);
+        criteria['loc.country'] = filterBy.region;
     }
+
     if (filterBy?.guestFavorite !== "false") {
-        console.log(filterBy.guestFavorite)
-        criteria.guestFavorite = { $ne: false }
+        console.log(filterBy.guestFavorite);
+        criteria.guestFavorite = { $ne: false };
     }
-    if (filterBy?.check_In && filterBy?.check_Out) {
-        criteria['orders.check_In'] = {
-            $lte: new Date(filterBy.checkOut).toISOString()
-        }
-        criteria['orders.check_Out'] = {
-            $gte: new Date(filterBy.checkIn).toISOString()
-        }
-    }
+
+    return criteria;
 }
+
 
 
 async function getById(stayId) {
